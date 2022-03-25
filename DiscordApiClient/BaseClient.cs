644841919185cbcd23dcf33044445b9681363d51
Jan Kahnt <1987jfk@googlemail.com>
2022-03-25 10:49:13 +0100

    [ADD] Basic classes

---
+++ DiscordApiClient/BaseClient.cs
@@ -0,0 +1,19 @@
+﻿namespace DiscordApiClient
+{
+    /// <summary>
+    /// Base client class for initialization and main logic
+    /// </summary>
+    public partial class DiscordClient
+    {
+        DiscordClientConfig _config;
+
+        /// <summary>
+        /// Base constructor
+        /// </summary>
+        /// <param name="config">Configuration object</param>
+        public DiscordClient(DiscordClientConfig config)
+        {
+            _config = config;
+        }
+    }
+}
---
+++ DiscordApiClient/DiscordClientConfig.cs
@@ -0,0 +1,39 @@
+﻿namespace DiscordApiClient
+{
+    /// <summary>
+    /// Class for configuration settings of the client
+    /// </summary>
+    public class DiscordClientConfig
+    {
+        private readonly string _apiKey;
+
+        /// <summary>
+        /// Base constructor
+        /// </summary>
+        /// <param name="apiKey">API key needed for access</param>
+        public DiscordClientConfig(string apiKey)
+        {
+            _apiKey = apiKey;
+        }
+
+        /// <summary>
+        /// Extended constructur to additionally sét the guild id
+        /// </summary>
+        /// <param name="apiKey">API key needed for access</param>
+        /// <param name="guildId">ID of the guild to access</param>
+        public DiscordClientConfig(string apiKey, string guildId) : this(apiKey)
+        {
+            GuildId = guildId;
+        }
+
+        /// <summary>
+        /// Property to retrieve the api key
+        /// </summary>
+        public string ApiKey { get { return _apiKey; } }
+
+        /// <summary>
+        /// Optinional property to set the guild id
+        /// </summary>
+        public string? GuildId { get; set; }
+    }
+}
