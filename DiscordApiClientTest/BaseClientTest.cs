5c946ed4041d62a09066ce80008a05b973dd2bc0
jfk1987 <1987jfk@googlemail.com>
2022-03-25 15:18:11 +0100

    [CHANGE] Added tests for new property

--- DiscordApiClientTest/BaseClientTest.cs
+++ DiscordApiClientTest/BaseClientTest.cs
@@ -8,11 +8,16 @@ namespace DiscordApiClientTest
         [Fact]
         public void ConstructorTest()
         {
+            var newGuildId = "456";
             var config01 = new DiscordClientConfig("123");
 
             var client01 = new DiscordClient(config01);
 
             Assert.NotNull(client01);
+            Assert.Equal(config01, client01.Config);
+
+            client01.Config.GuildId = newGuildId;
+            Assert.Equal(newGuildId, config01.GuildId);
         }
     }
 }
