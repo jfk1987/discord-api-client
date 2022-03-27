27a2e2081d1bbe3408bd224a70ba245f641898fa
jfk1987 <1987jfk@googlemail.com>
2022-03-27 23:12:46 +0200

    [CHANGE] Implemented basic methods

--- DiscordApiClient/Clients/GuildScheduledEventClient.cs
+++ DiscordApiClient/Clients/GuildScheduledEventClient.cs
@@ -17,8 +17,26 @@ namespace DiscordApiClient.Clients
 
             var response = await _client.GetRequestAsync(request);
 
-            return null;
+            return Deserialize<List<GuildScheduledEvent>>(response.Body);
         }
+
+        public async Task<GuildScheduledEvent> CreateGuildScheduledEventAsync(GuildScheduledEvent newEvent, string guildId = null)
+        {
+            CheckGuildId(ref guildId);
+
+            string path = $"/guilds/{guildId}/scheduled-events";
+
+            var request = new RestRequest
+            {
+                Body = Serialize(newEvent),
+                Path = path
+            };
+
+            var response = await _client.GetRequestAsync(request);
+
+            return Deserialize<GuildScheduledEvent>(response.Body);
+        }
+
 
         private void CheckGuildId(ref string guildId)
         {
